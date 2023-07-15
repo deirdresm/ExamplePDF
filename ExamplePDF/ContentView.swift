f3753f37cca8fb03a48b39133af630917f3b9c67
Deirdre Saoirse Moen <deirdre@deirdre.net>
2023-07-14 17:35:59 -0700

    Initial Commit

---
+++ ExamplePDF/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ExamplePDF
+//
+//  Created by Deirdre Saoirse Moen on 7/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExamplePDF/ExamplePDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamplePDFApp.swift
+//  ExamplePDF
+//
+//  Created by Deirdre Saoirse Moen on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ExamplePDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
